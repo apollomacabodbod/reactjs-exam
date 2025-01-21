c02a24cb5d05c98770ea474f4d89572a9b1fd9c7
apollo <apollomacabodbod8@gmail.com>
2025-01-21 16:48:53 +0800

    added todo items

---
+++ src/home/_components/home-todo-items.tsx
@@ -0,0 +1,57 @@
+
+
+
+import { useState } from "react"
+
+export default function HomeTodoItems(){
+
+
+
+    type todoTypes = {
+        
+        id: number;
+        Title: string;
+        date_added: string
+        date_completed: string
+    }
+
+
+    const [todo, setTodo] = useState<todoTypes[]>([
+
+      {id: 1, Title: "todo1", date_added:"jan22", date_completed: "jan22" },
+      {id: 2, Title: "todo2", date_added:"jan22", date_completed: "jan22" },
+      {id: 3, Title: "todo2", date_added:"jan22", date_completed: "jan22" },
+      {id: 4, Title: "todo3", date_added:"jan22", date_completed: "jan22" },
+
+    ]
+    )
+
+
+
+    return(<>
+
+
+      <div className="grid grid-cols-1">
+
+        {todo.map(() => (
+
+
+          <div className="flex flex-col">
+
+
+
+          </div>
+
+
+
+        ))}
+
+
+
+
+
+      </div>
+    
+    
+    </>)
+}
--- src/home/_components/home-todo-list.tsx
+++ src/home/_components/home-todo-list.tsx
@@ -3,6 +3,8 @@
 
 export default function HomeTodoList(){
 
+    
+
 
   return(<>
 
